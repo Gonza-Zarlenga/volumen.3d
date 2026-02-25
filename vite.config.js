06b97e9372fc9f9aab3249b63cd1a364082f8d5c
Gonza-Zarlenga <gzarlenga95@gmail.com>
2026-02-25 00:49:05 -0300

    Fix __dirname in ESM config for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
  import react from '@vitejs/plugin-react'
  import tailwindcss from '@tailwindcss/vite'
  import { resolve } from 'path'
-
  import { fileURLToPath } from 'url'
  
  // https://vite.dev/config/
@@ -10,7 +9,8 @@
      plugins: [react(), tailwindcss()],
      build: {
          rollupOptions: {
-             input: {main: resolve(new URL('.', import.meta.url).pathname, 'index.html'), main: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html'),
+             input: {
+                main: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html'),
              },
          },
      },
